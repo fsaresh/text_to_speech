9868da23be4cc23e5431eec643213f71aba2b321
Farhan Sareshwala <p.git@fsaresh.com>
2023-07-12 13:33:22 -0700

    Avoid opening speech.txt unless needed -- find_voices uses sample text

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from helper import find_voices, speak_text
+from voices import find_voices, speak_text
 
-# See helper.py and find_voices function for other voices
+# See voices.py and find_voices function for other voices
 VOICE_INDEX = 7
 
 
 def main():
+    # To pick a voice, uncomment the below line and listen through the options
+    # find_voices(speech_to_say="sample text to say", language='en')
+
     with open("speech.txt") as file:
         speech_to_say = file.read()
-
-        # find_voices(speech_to_say="sample text to say", language='en')
         speak_text(speech_to_say=speech_to_say, voice_index_to_use=VOICE_INDEX)
 
 
